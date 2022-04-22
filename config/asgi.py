29d22f65aaceb33011f3db06f1df3b0b28721cec
ramiboutas <ramiboutas@hotmail.com>
2022-04-22 21:23:11 +0200

    dotenv in wsgi and asgi

--- config/asgi.py
+++ config/asgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-
+import dotenv
 from django.core.asgi import get_asgi_application
 
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_asgi_application()
