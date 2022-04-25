9dbc9e146516225170bd9a4ef095bf80249652d1
ramiboutas <ramiboutas@hotmail.com>
2022-04-25 22:55:21 +0200

    static root

--- config/settings.py
+++ config/settings.py
@@ -144,6 +144,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = str(BASE_DIR.joinpath('static'))
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
