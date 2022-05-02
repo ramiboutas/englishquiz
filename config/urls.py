06e3cea1cab9069bb8a5e4d8caeaa5184e454b4c
ramiboutas <ramiboutas@hotmail.com>
2022-05-02 21:44:39 +0200

    robots.txt added

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quiz.urls')),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),),
 ]
