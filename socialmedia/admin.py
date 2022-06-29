b61d7de60dec75d164e023cbaac7972cd1f989e3
rami <ramiboutas@hotmail.com>
2022-06-29 14:50:11 +0200

    registed model in admin.py

--- socialmedia/admin.py
+++ socialmedia/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ScheduledSocialPost, RegularSocialPost
+
+
+admin.site.register(ScheduledSocialPost)
+admin.site.register(RegularSocialPost)
