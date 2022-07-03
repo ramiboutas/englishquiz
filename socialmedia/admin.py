1fef7e17e0f94d59ef745b41980899c5691ae82a
ramiboutas <ramiboutas@hotmail.com>
2022-07-03 18:45:19 +0200

    admin social media improved

--- socialmedia/admin.py
+++ socialmedia/admin.py
@@ -3,5 +3,18 @@ from django.contrib import admin
 from .models import ScheduledSocialPost, RegularSocialPost
 
 
-admin.site.register(ScheduledSocialPost)
-admin.site.register(RegularSocialPost)
+
+class ScheduledSocialPostAdmin(admin.ModelAdmin):
+    search_fields = ['text']
+    readonly_fields = ['created', 'updated']
+    list_filter = ['promote', 'created', 'updated']
+
+class RegularSocialPostAdmin(admin.ModelAdmin):
+    search_fields = ['text']
+    readonly_fields = ['created', 'updated', 'promoted']
+    list_filter = ['promote', 'created', 'updated', 'promoted']
+
+
+
+admin.site.register(ScheduledSocialPost, ScheduledSocialPostAdmin)
+admin.site.register(RegularSocialPost, RegularSocialPostAdmin)
