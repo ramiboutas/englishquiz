667b1e606e3a78822dff3920c61f699259bc4a5f
ramiboutas <ramiboutas@hotmail.com>
2022-07-10 02:59:17 +0200

    views and urls for linkedin  and twitter

--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -2,8 +2,11 @@
 
 from django.urls import path
 
-from .views import telegram_view
+from .views import telegram_view, linkedin_view, twitter_view
 
 urlpatterns = [
-    path('telegram/', telegram_view, name='socialmedia_telegram'),
+    path('telegram/', telegram_view, name='socialmedia_telegram'), 
+    path('linkedin/', linkedin_view, name='socialmedia_linkedin'), 
+    path('twitter/', twitter_view, name='socialmedia_twitter'), 
+    
 ]
