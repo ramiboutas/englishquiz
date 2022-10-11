d6bfa71cfddface9891127e7e72aa6f88f29e563
Rami Boutassghount <ramiboutas@hotmail.com>
2022-10-11 19:22:53 +0200

    admin nested inlines added

--- config/settings.py
+++ config/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     # 'tinymce',
     # 'newsletter',
     'newsfeed',
+    'nested_inline',
 
 ]
 
--- quiz/admin.py
+++ quiz/admin.py
@@ -2,58 +2,59 @@
 from django.contrib import admin
 from .models import Quiz, Lection, Question, Answer, DeeplLanguage, TranslatedQuestion
 
+from nested_inline.admin import NestedStackedInline, NestedModelAdmin
+
+
 class LectionInline(admin.StackedInline):
     model = Lection
-    extra = 1
-
-
-class QuestionInline(admin.StackedInline):
-    model = Question
     extra = 5
 
-
-class AnswerInline(admin.StackedInline):
-    model = Answer
-    extra = 3
-
-
+@admin.register(Quiz)
 class QuizAdmin(admin.ModelAdmin):
     search_fields = ['name']
     readonly_fields = ['views']
     list_filter = ['name']
+    prepopulated_fields = {"slug": ("name",)}
     inlines = [LectionInline,]
 
 
-class LectionAdmin(admin.ModelAdmin):
+class AnswerInline(NestedStackedInline):
+    model = Answer
+    extra = 3
+    fk_name = 'question'
+
+
+class QuestionInline(NestedStackedInline):
+    search_fields = ['text_one']
+    list_filter = ['lection']
+    readonly_fields = ['promoted', ]
+    model = Question
+    extra = 10
+    fk_name = 'lection'
+    inlines = [AnswerInline]
+
+
+@admin.register(Lection)
+class LectionAdmin(NestedModelAdmin):
     search_fields = ['name']
     readonly_fields = ['views']
     list_filter = ['name', 'quiz__name']
     prepopulated_fields = {"slug": ("name",)}
-    inlines = [QuestionInline,]
+    model = Quiz
+    inlines = [QuestionInline]
 
 
-class QuestionAdmin(admin.ModelAdmin):
-    search_fields = ['text_one']
-    list_filter = ['lection']
-    readonly_fields = ['promoted', ]
-    inlines = [AnswerInline,]
+# Translation models
 
-
+@admin.register(DeeplLanguage)
 class DeeplLanguageAdmin(admin.ModelAdmin):
     list_filter = ['supports_formality']
     readonly_fields = ['views', ]
     list_display = ['name', 'code', 'supports_formality', 'views']
 
 
+@admin.register(TranslatedQuestion)
 class TranslatedQuestionAdmin(admin.ModelAdmin):
     list_filter = ['language']
     list_display = ['original_text', 'language', 'created']
     readonly_fields = ['language', 'question', 'original_text', 'created', 'updated']
-
-
-
-admin.site.register(Quiz, QuizAdmin)
-admin.site.register(Lection, LectionAdmin)
-admin.site.register(Question, QuestionAdmin)
-admin.site.register(DeeplLanguage, DeeplLanguageAdmin)
-admin.site.register(TranslatedQuestion, TranslatedQuestionAdmin)
