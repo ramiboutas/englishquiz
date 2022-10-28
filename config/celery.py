551f6c72fda3e2998f689a5317ab71c0a7a84f13
Rami Boutassghount <ramiboutas@hotmail.com>
2022-10-28 14:52:20 +0200

    removed some comments

--- config/celery.py
+++ config/celery.py
@@ -6,13 +6,11 @@ from celery import Celery
 from django.apps import apps
 from django.conf import settings
 
-# import dotenv # django-dotenv
 from dotenv import load_dotenv  # python-dotenv
 
-# dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')) # django-dotenv
 load_dotenv(
     dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
-)  # python-dotenv
+)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
--- config/wsgi.py
+++ config/wsgi.py
@@ -13,13 +13,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# import dotenv # django-dotenv
 from dotenv import load_dotenv  # python-dotenv
 
-# dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')) # django-dotenv
 load_dotenv(
     dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
-)  # python-dotenv
+) 
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
