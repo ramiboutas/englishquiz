0dff88d16df747b1ee29939530c9f2964cb57f8d
ramiboutas <ramiboutas@hotmail.com>
2022-11-05 22:36:54 +0100

    moved celery settings to celery.py

--- config/celery.py
+++ config/celery.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 from django.apps import apps
 from django.conf import settings
 from dotenv import load_dotenv  # python-dotenv
@@ -13,10 +14,45 @@ load_dotenv(
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
+broker_url = "redis://127.0.0.1:6379/3"
+result_backend = "redis://127.0.0.1:6379/3"
+
+
 app = Celery("config")
 app.config_from_object(settings, namespace="CELERY")
 app.conf.timezone = settings.TIME_ZONE
 app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
+
+app.conf.beat_schedule = {
+    "share_random_question": {
+        "task": "socialmedia.tasks.share_random_question_instance",
+        "schedule": crontab(hour="10, 15", minute=00),
+        "options": {
+            "expires": 0,
+        },
+    },
+    "share_regular_social_post": {
+        "task": "socialmedia.tasks.share_regular_social_post",
+        "schedule": crontab(
+            hour=12, minute=30
+        ),  # when more instances available: add crontab(hour='8,13', minute=00)
+        "options": {
+            "expires": 0,
+        },
+    },
+    "send_email_newsletter": {
+        "task": "send_email_newsletter_task",
+        "schedule": crontab(minute=0, hour="*"),
+    },
+    "delete_responded_contact_instances": {
+        "task": "delete_responded_contact_instances",
+        "schedule": crontab(hour=3, minute=00),
+    },
+    "update_linkedin_company_page_access_token": {
+        "task": "update_linkedin_company_page_access_token",
+        "schedule": crontab(0, 0, day_of_month="1", month_of_year="1,3,5,7,9,11"),
+    },
+}
 
 
 @app.task(bind=True)
