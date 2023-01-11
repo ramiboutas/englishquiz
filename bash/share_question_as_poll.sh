a89153ba76b54bb606029672f78460db8e142368
rami <ramiboutas@hotmail.com>
2023-01-11 23:41:57 +0100

    sh cmd added for sharing poll

---
+++ bash/share_question_as_poll.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/rami/englishquiz
+source venv/bin/activate
+python manage.py question_as_poll
---
+++ socialmedia/commands/question_as_poll.py
@@ -0,0 +1,20 @@
+
+
+from django.core.management.base import BaseCommand
+from socialmedia.tasks import share_random_question_as_poll
+
+# from django.contrib.sites.models import Site
+
+
+class Command(BaseCommand):
+    help = "Shares a question as Poll to Social Media accounts."
+
+
+    def handle(self, *args, **options):
+
+        self.stdout.write("Sharing question as poll...")
+        
+        share_random_question_as_poll()
+
+        self.stdout.write("Done.")
+
