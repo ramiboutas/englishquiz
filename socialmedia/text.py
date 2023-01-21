5769540051a9a9fb7b5bb8af9d69e04aa4b88011
rami <ramiboutas@hotmail.com>
2023-01-21 09:43:18 +0100

    break lines added to get_poll_explanation_text

--- socialmedia/text.py
+++ socialmedia/text.py
@@ -35,7 +35,7 @@ def get_poll_explanation_text(question_obj):
         "What goes in the gap?",
     ]
     text = random.choice(text_options)
-    text += " Check you answer here: "
+    text += "\n\nCheck you answer here 👉 "
     text += f"https://www.englishstuff.online{question_obj.get_detail_url()}"
     
     return text
