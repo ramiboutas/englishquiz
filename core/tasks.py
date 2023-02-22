c6520a26106c866efd1b2ac3501e7894c65521a8
rami <ramiboutas@hotmail.com>
2023-02-22 08:50:11 +0100

    slight change

--- core/tasks.py
+++ core/tasks.py
@@ -6,7 +6,7 @@ from newsfeed.models import Subscriber
 @shared_task(bind=True)
 def remove_unverified_newsletter_subscribers(self, **kwargs):
     qs = Subscriber.objects.filter(
-        verification_sent_date__lt=timezone.now() - timezone.timedelta(days=7),
+        verification_sent_date__lt=timezone.now() - timezone.timedelta(days=4),
         verified = False,
         subscribed = False,
         )
