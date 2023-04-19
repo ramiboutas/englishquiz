586a7937899756b37ed748fff6f1dcaaed8a2d7c
rami <ramiboutas@hotmail.com>
2023-04-19 19:07:48 +0200

    cmd added

---
+++ core/management/commands/create_deny_ips.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+
+from request.models import Request
+
+
+DENY_IPS_WITH_PATHS = [
+    "/wp-login.php",
+    "/.env",
+    "//wp1/wp-includes/wlwmanifest.xml",
+    "//2019/wp-includes/wlwmanifest.xml",
+    "//web/wp-includes/wlwmanifest.xml",
+    "//wp/wp-includes/wlwmanifest.xml",
+    "//site/wp-includes/wlwmanifest.xml",
+]
+
+
+class Command(BaseCommand):
+    help = "Seed database with sample data."
+
+    def handle(self, *args, **options):
+        requests = Request.objects.filter(path__in=DENY_IPS_WITH_PATHS)
+        for request in requests:
+            output = f"deny {request.ip};"
+        self.stdout.write(output)
