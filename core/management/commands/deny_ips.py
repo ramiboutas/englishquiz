41f1f369f0afb5a901dea855014a2e7e924ac5e1
rami <ramiboutas@hotmail.com>
2023-04-20 08:49:36 +0200

    deny ips contains query

--- core/management/commands/deny_ips.py
+++ core/management/commands/deny_ips.py
@@ -1,6 +1,9 @@
+import operator
 from pathlib import Path
+from functools import reduce
 
 from django.conf import settings
+from django.db.models import Q
 from django.core.management.base import BaseCommand
 
 from request.models import Request
@@ -12,7 +15,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         output = ""
         file = Path(settings.NGIX_DENY_CONFIGURATION_FILE)
-        requests = Request.objects.filter(path__in=settings.DENY_IPS_WITH_PATHS)
+
+        requests = Request.objects.filter(reduce(operator.or_, (Q(path__contains=x)
+                                          for x in settings.DENY_IPS_WITH_PATHS)))
+
         with open(file, "r") as f:
             current = f.read()
 
