dba4097fb6a4bf377e4f0c8b7a903c2fc2b8901c
Rami <ramiboutas@hotmail.com>
2023-10-22 22:42:18 +0200

    cors

--- config/settings.py
+++ config/settings.py
@@ -380,6 +380,11 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = "Access-Control-Allow-Origin"
 
+
+CORS_ALLOWED_ORIGINS = [
+    "https://pagead2.googlesyndication.com",
+]
+
 if HTTPS:  # pragma: no cover
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
     SECURE_HSTS_SECONDS = 31_536_000  # 31536000 # usual: 31536000 (1 year)
